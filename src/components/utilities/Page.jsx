aa42f3e1c4de876957ac455910250a6cb6a30210
MISHUMO TROY LUMADI <59746304+Mishumo-Troy-Lumadi@users.noreply.github.com>
2022-06-17 15:26:13 +0200

    Update Page.jsx

--- src/components/utilities/Page.jsx
+++ src/components/utilities/Page.jsx
@@ -9,7 +9,7 @@ function Page({ title='', description='', favicon = '/favicon.ico', as='column',
                 <meta name="description" content={description} />
                 <link rel="icon" href={favicon} />
             </Head>
-            <Layout {...{ as }} className={`w-screen h-screen overflow-y-auto ${className}`}>
+            <Layout {...{ as }} className={`w-screen h-screen overflow-hidden ${className}`}>
                 {children}
             </Layout>
         </>
