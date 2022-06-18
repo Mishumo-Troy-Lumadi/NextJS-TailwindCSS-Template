c60751861f4b4fe127ac0c67efddefe3396bf634
MISHUMO TROY LUMADI <59746304+Mishumo-Troy-Lumadi@users.noreply.github.com>
2022-06-18 06:21:16 +0200

    Home Page Template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import { Page } from '@components/utilities'
+
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <Page title='Hello World' description='Your First NextJS Page' as='column' className='justify-center items-center'>
+      <h1>Hello World</h1>
+    </Page>
   )
 }
