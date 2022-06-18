31c058c4c27de223b9e2e38d854689910581aa74
MISHUMO TROY LUMADI <59746304+Mishumo-Troy-Lumadi@users.noreply.github.com>
2022-06-18 14:42:26 +0200

    Whitespace

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import { Page } from '@components/utilities'
 
 export default function Home() {
-  return (
-    <Page title='Hello World' description='Your First NextJS Page' as='column' className='justify-center items-center'>
-      <h1>Hello World</h1>
-    </Page>
-  )
+	return (
+		<Page title='Hello World' description='Your First NextJS Page' as='column' className='justify-center items-center'>
+			<h1>Hello World</h1>
+		</Page>
+	)
 }
